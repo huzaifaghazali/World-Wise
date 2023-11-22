39b1a919da6485665a010e8ffe3397522d19d1da
huzaifaghazali <huzaifaghazali@gmail.com>
2023-11-22 20:27:46 +0500

    Added city list

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ import Login from './pages/Login';
 import CityList from './components/CityList';
 import { useEffect, useState } from 'react';
 
-const BASE_URL = 'http://localhost:8000/cities';
+const BASE_URL = 'http://localhost:8000';
 
 function App() {
-  const [cities, setCities] = useState({});
+  const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
@@ -39,8 +39,14 @@ function App() {
         <Route path='pricing' element={<Pricing />} />
         <Route path='login' element={<Login />} />
         <Route path='app' element={<AppLayout />}>
-          <Route index element={<CityList />} />
-          <Route path='cities' element={<CityList />} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path='cities'
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path='countries' element={<p>Countries</p>} />
           <Route path='form' element={<p>Form</p>} />
         </Route>
